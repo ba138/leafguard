a95644ee3701b45b05c46a61a5535df8ce111b2c
Basit ali <basitalyshah51214@gmail.com>
2025-10-27 02:17:08 -0700

    fix the ui issues in the login and signup screen

--- lib/views/auth_views/login_screen.dart
+++ lib/views/auth_views/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:leafguard/const/colors.dart';
@@ -12,121 +14,170 @@ class LoginScreen extends ConsumerStatefulWidget {
   const LoginScreen({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _LoginScreenState();
+  ConsumerState<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends ConsumerState<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
+      resizeToAvoidBottomInset: true, // ✅ prevents overflow
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              CommonImageView(
-                imagePath: 'images/logo.png',
-                height: 120,
-                width: 120,
-              ),
-              SizedBox(height: 20),
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: AppColors.secondary,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(60),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.06,
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            CommonImageView(
+              imagePath: 'images/logo.png',
+              height: 120,
+              width: 120,
+            ),
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(60)),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 6, sigmaY: 6), // Glass blur
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: AppColors.secondary, // translucent
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(60),
                       ),
-                      MyText(
-                        text: 'Welcome Back',
-                        size: 24,
-                        weight: FontWeight.bold,
-                        color: AppColors.primary,
+                      border: Border.all(
+                        color: Colors.white.withValues(alpha: 0.2),
+                        width: 1.5, // subtle white border
                       ),
-                      MyText(
-                        text: 'Please login to your account',
-                        size: 16,
-                        color: AppColors.primary,
-                      ),
-                      SizedBox(height: 24),
-                      MyTextField(label: 'Email', hint: 'Enter your email'),
-                      MyTextField(
-                        label: 'Password',
-                        hint: 'Enter your password',
-                        isObSecure: true,
-                        suffix: InkWell(
-                          onTap: () {},
-                          child: Icon(Icons.visibility_off),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withValues(alpha: 0.1),
+                          blurRadius: 6,
+                          offset: Offset(0, 4),
                         ),
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: MyText(
-                          text: 'Forgot Password?',
-                          color: AppColors.primary,
-                        ),
-                      ),
-                      const SizedBox(height: 40),
-                      MyButton(text: 'Login', onPressed: () {}),
-                      const SizedBox(height: 12),
-                      MyGoogleButton(
-                        title: 'Continue with Google',
-                        path: 'images/google.png',
-                        onPressed: () {},
-                      ),
-                      const SizedBox(height: 8),
-                      Row(
-                        children: [
-                          Expanded(child: Divider(color: AppColors.primary)),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 8.0,
-                            ),
-                            child: MyText(text: 'OR', color: AppColors.primary),
-                          ),
-                          Expanded(child: Divider(color: AppColors.primary)),
-                        ],
-                      ),
-                      const SizedBox(height: 8),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          MyText(
-                            text: "Don't have an account?",
-                            color: AppColors.primary,
-                          ),
-                          const SizedBox(width: 4),
-                          MyText(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => const SignupScreen(),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: LayoutBuilder(
+                        builder: (context, constraints) {
+                          return SingleChildScrollView(
+                            physics: BouncingScrollPhysics(),
+                            child: ConstrainedBox(
+                              constraints: BoxConstraints(
+                                minHeight: constraints.maxHeight,
+                              ),
+                              child: IntrinsicHeight(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                          0.06,
+                                    ),
+                                    MyText(
+                                      text: 'Welcome Back',
+                                      size: 24,
+                                      weight: FontWeight.bold,
+                                      color: AppColors.primary,
+                                    ),
+                                    MyText(
+                                      text: 'Please login to your account',
+                                      size: 16,
+                                      color: AppColors.primary,
+                                    ),
+                                    SizedBox(height: 24),
+                                    MyTextField(
+                                      label: 'Email',
+                                      hint: 'Enter your email',
+                                    ),
+                                    MyTextField(
+                                      label: 'Password',
+                                      hint: 'Enter your password',
+                                      isObSecure: true,
+                                      suffix: InkWell(
+                                        onTap: () {},
+                                        child: Icon(Icons.visibility_off),
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.centerRight,
+                                      child: MyText(
+                                        text: 'Forgot Password?',
+                                        color: AppColors.primary,
+                                      ),
+                                    ),
+                                    SizedBox(height: 40),
+                                    MyButton(text: 'Login', onPressed: () {}),
+                                    SizedBox(height: 12),
+                                    MyGoogleButton(
+                                      title: 'Continue with Google',
+                                      path: 'images/google.png',
+                                      onPressed: () {},
+                                    ),
+                                    SizedBox(height: 8),
+                                    Row(
+                                      children: [
+                                        Expanded(
+                                          child: Divider(
+                                            color: AppColors.primary,
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.symmetric(
+                                            horizontal: 8.0,
+                                          ),
+                                          child: MyText(
+                                            text: 'OR',
+                                            color: AppColors.primary,
+                                          ),
+                                        ),
+                                        Expanded(
+                                          child: Divider(
+                                            color: AppColors.primary,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(height: 8),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        MyText(
+                                          text: "Don't have an account?",
+                                          color: AppColors.primary,
+                                        ),
+                                        SizedBox(width: 4),
+                                        MyText(
+                                          onTap: () {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                builder: (context) =>
+                                                    SignupScreen(),
+                                              ),
+                                            );
+                                          },
+                                          text: 'Sign Up',
+                                          color: AppColors.primary,
+                                          weight: FontWeight.bold,
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(height: 20),
+                                  ],
                                 ),
-                              );
-                            },
-                            text: 'Sign Up',
-                            color: AppColors.primary,
-                            weight: FontWeight.bold,
-                          ),
-                        ],
+                              ),
+                            ),
+                          );
+                        },
                       ),
-                      const SizedBox(height: 20),
-                    ],
+                    ),
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/views/auth_views/signup_screen.dart
+++ lib/views/auth_views/signup_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:leafguard/const/colors.dart';
 import 'package:leafguard/views/widget/common_image_view.dart';
@@ -23,128 +24,148 @@ class SignupScreen extends StatelessWidget {
               width: 80,
             ),
             Expanded(
-              child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: AppColors.secondary,
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(60)),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16.0),
-                  child: LayoutBuilder(
-                    builder: (context, constraints) {
-                      return SingleChildScrollView(
-                        physics: BouncingScrollPhysics(),
-                        child: ConstrainedBox(
-                          constraints: BoxConstraints(
-                            minHeight: constraints.maxHeight,
-                          ),
-                          child: IntrinsicHeight(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(height: 40),
-                                MyText(
-                                  text: 'Welcome Onboard',
-                                  size: 20,
-                                  weight: FontWeight.bold,
-                                  color: AppColors.primary,
-                                ),
-                                MyText(
-                                  text: 'Please Create your account',
-                                  size: 14,
-                                  color: AppColors.primary,
-                                ),
-                                SizedBox(height: 12),
-
-                                MyTextField(
-                                  label: 'Username',
-                                  hint: 'Enter username',
-                                ),
-                                MyTextField(
-                                  label: 'Email',
-                                  hint: 'Enter your email',
-                                ),
-                                MyTextField(
-                                  label: 'Password',
-                                  hint: 'Enter your password',
-                                  isObSecure: true,
-                                  suffix: InkWell(
-                                    onTap: () {},
-                                    child: Icon(Icons.visibility_off),
-                                  ),
-                                ),
-                                MyTextField(
-                                  label: 'Confirm Password',
-                                  hint: 'Re-enter your password',
-                                  isObSecure: true,
-                                  suffix: InkWell(
-                                    onTap: () {},
-                                    child: Icon(Icons.visibility_off),
-                                  ),
-                                ),
-
-                                SizedBox(height: 20),
-                                MyButton(text: 'Sign Up', onPressed: () {}),
-                                SizedBox(height: 12),
-
-                                MyGoogleButton(
-                                  title: 'Continue with Google',
-                                  path: 'images/google.png',
-                                  onPressed: () {},
-                                ),
-                                SizedBox(height: 8),
-
-                                Row(
-                                  children: [
-                                    Expanded(
-                                      child: Divider(
-                                        color: AppColors.primary,
-                                        thickness: 1,
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: EdgeInsets.symmetric(
-                                        horizontal: 8.0,
-                                      ),
-                                      child: MyText(
-                                        text: 'OR',
-                                        color: AppColors.primary,
-                                      ),
-                                    ),
-                                    Expanded(
-                                      child: Divider(
-                                        color: AppColors.primary,
-                                        thickness: 1,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                SizedBox(height: 8),
-
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    MyText(
-                                      text: "Already have an account?",
-                                      color: AppColors.primary,
-                                    ),
-                                    SizedBox(width: 4),
-                                    MyText(
-                                      onTap: () => Navigator.pop(context),
-                                      text: 'Sign In',
-                                      color: AppColors.primary,
-                                      weight: FontWeight.bold,
-                                    ),
-                                  ],
-                                ),
-                                SizedBox(height: 20),
-                              ],
-                            ),
-                          ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(60)),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 6, sigmaY: 6), // glass blur
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: AppColors.secondary, // translucent
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(60),
+                      ),
+                      border: Border.all(
+                        color: Colors.white.withValues(alpha: 0.2),
+                        width: 1.5,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withValues(alpha: 0.1),
+                          blurRadius: 6,
+                          offset: Offset(0, 4),
                         ),
-                      );
-                    },
+                      ],
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: LayoutBuilder(
+                        builder: (context, constraints) {
+                          return SingleChildScrollView(
+                            physics: BouncingScrollPhysics(),
+                            child: ConstrainedBox(
+                              constraints: BoxConstraints(
+                                minHeight: constraints.maxHeight,
+                              ),
+                              child: IntrinsicHeight(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 40),
+                                    MyText(
+                                      text: 'Welcome Onboard',
+                                      size: 20,
+                                      weight: FontWeight.bold,
+                                      color: AppColors.primary,
+                                    ),
+                                    MyText(
+                                      text: 'Please Create your account',
+                                      size: 14,
+                                      color: AppColors.primary,
+                                    ),
+                                    SizedBox(height: 12),
+
+                                    MyTextField(
+                                      label: 'Username',
+                                      hint: 'Enter username',
+                                    ),
+                                    MyTextField(
+                                      label: 'Email',
+                                      hint: 'Enter your email',
+                                    ),
+                                    MyTextField(
+                                      label: 'Password',
+                                      hint: 'Enter your password',
+                                      isObSecure: true,
+                                      suffix: InkWell(
+                                        onTap: () {},
+                                        child: Icon(Icons.visibility_off),
+                                      ),
+                                    ),
+                                    MyTextField(
+                                      label: 'Confirm Password',
+                                      hint: 'Re-enter your password',
+                                      isObSecure: true,
+                                      suffix: InkWell(
+                                        onTap: () {},
+                                        child: Icon(Icons.visibility_off),
+                                      ),
+                                    ),
+
+                                    SizedBox(height: 20),
+                                    MyButton(text: 'Sign Up', onPressed: () {}),
+                                    SizedBox(height: 12),
+
+                                    MyGoogleButton(
+                                      title: 'Continue with Google',
+                                      path: 'images/google.png',
+                                      onPressed: () {},
+                                    ),
+                                    SizedBox(height: 8),
+
+                                    Row(
+                                      children: [
+                                        Expanded(
+                                          child: Divider(
+                                            color: AppColors.primary,
+                                            thickness: 1,
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.symmetric(
+                                            horizontal: 8.0,
+                                          ),
+                                          child: MyText(
+                                            text: 'OR',
+                                            color: AppColors.primary,
+                                          ),
+                                        ),
+                                        Expanded(
+                                          child: Divider(
+                                            color: AppColors.primary,
+                                            thickness: 1,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(height: 8),
+
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        MyText(
+                                          text: "Already have an account?",
+                                          color: AppColors.primary,
+                                        ),
+                                        SizedBox(width: 4),
+                                        MyText(
+                                          onTap: () => Navigator.pop(context),
+                                          text: 'Sign In',
+                                          color: AppColors.primary,
+                                          weight: FontWeight.bold,
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(height: 20),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
                   ),
                 ),
               ),
